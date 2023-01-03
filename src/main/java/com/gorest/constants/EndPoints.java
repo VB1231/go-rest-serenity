ce017fc64b62e2fef412c3162e0a074245d0ba8f
vb1231 <viral2627@gmail.com>
2023-01-03 14:16:48 +0000

    go-rest-serenity

---
+++ src/main/java/com/gorest/constants/EndPoints.java
@@ -0,0 +1,23 @@
+package com.gorest.constants;
+
+/**
+ * Created by Jay
+ */
+public class EndPoints {
+
+    /**
+     * This is Endpoints of go rest api
+     */
+    public static final String GET_ALL_USERS = "/users";
+
+    public static final String GET_SINGLE_USER_BY_ID = "/{id}";
+
+    public static final String UPDATE_USER_BY_ID="/{id}";
+
+    public static final String DELETE_USER_BY_ID = "/{id}";
+
+    /**
+     * This is Endpoints of Authentication api
+     */
+        public static final String Authorization = "Bearer 600f4364266ef9256401822c412cbfa2a4fe3c13c5c708bf2206cbb120f2a4c9";
+}
